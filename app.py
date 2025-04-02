87f2c3561f52c12990c4cc57ed91815bd8c1ed55
Bhuvanesh Singla <125354611+Bhuvanesh-Singla@users.noreply.github.com>
2025-04-02 10:31:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,18 @@ def get_image_as_base64(image_path):
 
 background_image_path = r"bigmap.jpg"
 set_background(get_image_as_base64(background_image_path))
+# st.markdown("""
+#     <style>
+#         footer {visibility: hidden;}
+#         header {visibility: hidden;}
+#     </style>
+# """, unsafe_allow_html=True)
+
 st.markdown("""
     <style>
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
+        footer {visibility: hidden !important;}
+        header {visibility: hidden !important;}
+        [data-testid="stFooter"] {display: none !important;}
     </style>
 """, unsafe_allow_html=True)
 
